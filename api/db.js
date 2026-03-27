ea23ce6160d73a45f53ad37cabef8a22a5d0445c
akramovbek4884 <akramov.bek4884@gmail.com>
2026-03-27 19:29:52 +0300

    fix: unconditional TLS bypass to resolve Vercel connection issues

--- api/db.js
+++ api/db.js
@@ -1,7 +1,5 @@
 import 'dotenv/config';
-if (process.env.NODE_ENV !== 'production') {
-    process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
-}
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 import pkg from 'pg';
 const { Pool } = pkg;
 
