24d49ec5d17148d9b66fa952084230074121e154
akramovbek4884 <akramov.bek4884@gmail.com>
2026-03-27 11:37:42 +0300

    Fix: Final password update with no special characters

--- api/db.js
+++ api/db.js
@@ -2,7 +2,7 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 import pkg from 'pg';
 const { Pool } = pkg;
 
-const connectionString = process.env.DATABASE_URL || 'postgresql://postgres.unfjcyajqahsmxcptgno:Planusa779744445%26@aws-1-ap-southeast-2.pooler.supabase.com:6543/postgres?sslmode=require';
+const connectionString = process.env.DATABASE_URL || 'postgresql://postgres.unfjcyajqahsmxcptgno:Planusa2026Market@aws-1-ap-southeast-2.pooler.supabase.com:6543/postgres?sslmode=require';
 
 const pool = new Pool({
     connectionString: connectionString,
